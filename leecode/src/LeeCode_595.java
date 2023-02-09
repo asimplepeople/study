a901624566d975e0aa309b6b77bb85a7d91a8a0d
xiezhaohui <xzhonly@163.com>
2023-02-09 13:50:45 +0800

    leecode 595 答题完成

---
+++ leecode/src/LeeCode_595.java
@@ -0,0 +1,9 @@
+/**
+ * https://leetcode.cn/problems/big-countries/
+ *
+ * @author xiezhaohui
+ * @date 2023/2/9 13:50
+ */
+public class LeeCode_595 {
+    // select name, population, area from world where population >= 25000000 or area >= 3000000
+}
