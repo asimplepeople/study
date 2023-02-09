0f460b00a5da19aa74784b95e0729863f830888b
xiezhaohui <xzhonly@163.com>
2023-02-09 13:40:48 +0800

    leecode 1797 答题完成

--- leecode/src/LeeCode_1797.java
+++ leecode/src/LeeCode_1797.java
@@ -1,3 +1,7 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 /**
  * https://leetcode.cn/problems/design-authentication-manager/
  *
@@ -7,5 +11,28 @@
 public class LeeCode_1797 {
     public static void main(String[] args) {
 
+    }
+    class AuthenticationManager {
+
+        private final Integer timeTolive;
+        Map<String, Integer> map = new HashMap();
+
+        public AuthenticationManager(int timeToLive) {
+            this.timeTolive = timeToLive;
+        }
+
+        public void generate(String tokenId, int currentTime) {
+            map.put(tokenId, currentTime);
+        }
+
+        public void renew(String tokenId, int currentTime) {
+            if(map.containsKey(tokenId) &&  currentTime >= map.get(tokenId) && currentTime - map.get(tokenId) <  timeTolive){
+                map.put(tokenId, currentTime);
+            }
+        }
+
+        public int countUnexpiredTokens(int currentTime) {
+            return (int)map.values().stream().filter(i -> currentTime >= i && currentTime - i < timeTolive).count();
+        }
     }
 }
