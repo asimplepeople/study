0847d55b6b381ccfeadc1641f2466ad906727304
xiezhaohui <xzhonly@163.com>
2023-02-09 14:01:59 +0800

    leecode 183 答题完成

---
+++ leecode/src/LeeCode_183.java
@@ -0,0 +1,9 @@
+/**
+ * https://leetcode.cn/problems/customers-who-never-order/
+ *
+ * @author xiezhaohui
+ * @date 2023/2/9 14:01
+ */
+public class LeeCode_183 {
+    // select a.name Customers  from Customers a left join orders b on a.id = b.customerId where b.id is null
+}
