b0bf4aab04c4fc05c11945b8945cceff0e46633c
xiezhaohui <xzhonly@163.com>
2023-02-09 13:53:01 +0800

    leecode 1757 答题完成

---
+++ leecode/src/LeeCode_1757.java
@@ -0,0 +1,9 @@
+/**
+ * https://leetcode.cn/problems/recyclable-and-low-fat-products/
+ *
+ * @author xiezhaohui
+ * @date 2023/2/9 13:52
+ */
+public class LeeCode_1757 {
+    // select product_id   from products where low_fats = 'Y' and recyclable = 'Y'
+}
