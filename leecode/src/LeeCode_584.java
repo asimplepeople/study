74f73058980d6c5d40f2575cf65f538704b97d30
xiezhaohui <xzhonly@163.com>
2023-02-09 13:57:36 +0800

    leecode 584 答题完成

---
+++ leecode/src/LeeCode_584.java
@@ -0,0 +1,10 @@
+/**
+ * https://leetcode.cn/problems/find-customer-referee/
+ * 在用mysql != 的时候会把 null排除在外
+ *
+ * @author xiezhaohui
+ * @date 2023/2/9 13:56
+ */
+public class LeeCode_584 {
+    // select name from customer where referee_id != 2 or referee_id is null
+}
